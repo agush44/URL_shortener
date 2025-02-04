4a800dc287aac02ea570cb9d96d176a4d77e7eb9
agush44 <magustinabenitez@gmail.com>
2025-02-04 12:56:13 -0300

    fix: env

--- Backend/index.js
+++ Backend/index.js
@@ -4,10 +4,9 @@ import { connectDB } from "./src/config/mongo.js";
 import { urlRoutes } from "./src/routes/urlRoutes.js";
 import errorMiddleware from "./src/middleware/errorMiddleware.js";
 
-process.loadEnvFile();
-
-const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
+
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
